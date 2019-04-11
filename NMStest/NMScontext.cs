810c92e836d4550a939ad80b143073656f72a5d3
James Spencer <jspencer@idealintegrations.net>
2019-04-11 15:50:42 -0400

    Add project files.

---
+++ NMStest/NMScontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NMStest.Data
+{
+    public class NMScontext: DbContext
+    {
+        public NMScontext(DbContextOptions<NMScontext> options) : base(options)
+        {
+            
+        }
+        public DbSet<NMStest.Models.Planet> Planet { get; set; }
+        public DbSet<NMStest.Models.Resource> Resource { get; set; }
+        public DbSet<NMStest.Models.StarSystems> StarSystems { get; set; }
+
+    }
+}
